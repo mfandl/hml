cbedae6d2f94179dda037c4b150c0de86f4fa1cb
Matej Fandl <fandl@mladypes.sk>
2020-03-30 18:48:52 +0200

    hopfiled net (hmatrix version) can now learn multiple patterns

--- src/HopfieldMat.hs
+++ src/HopfieldMat.hs
@@ -10,6 +10,13 @@ pat = fromList
   , -1, -1,  1, -1, -1
   , -1, -1,  1, -1, -1]
 
+pat2 :: Vector R
+pat2 = fromList
+  [  1, -1, -1, -1,  1
+  ,  1, -1, -1, -1,  1
+  ,  1,  1,  1,  1,  1
+  ,  1, -1, -1, -1,  1
+  ,  1, -1, -1, -1,  1]
 
 noisy :: Vector R
 noisy =  fromList
@@ -19,12 +26,20 @@ noisy =  fromList
   ,  1, -1, -1, -1, -1
   , -1, -1,  1, -1, -1]
 
+noisy2 :: Vector R
+noisy2 = fromList
+  [  1, -1, -1, -1,  1
+  ,  1, -1,  1, -1,  1
+  , -1,  1,  1,  1,  1
+  ,  1, -1, -1,  1,  1
+  ,  1, -1, -1, -1,  1]
+
 initialWeights :: Vector R -> Matrix R
 initialWeights input = let ln = size input
                         in (ln><ln) $ repeat 0
 
-train :: Vector R -> Matrix R 
-train p = (outer p p) * zeroDiagonal
+train :: Vector R -> Matrix R -> Matrix R
+train p w = (outer p p) * zeroDiagonal + w
   where
     zeroDiagonal = (sp><sp)(repeat 1) - ident sp 
     sp = size p
