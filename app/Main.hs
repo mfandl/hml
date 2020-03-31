b53b13410e3ef44bbdfe02ab7b11718d5cfe146e
Matej Fandl <fandl@mladypes.sk>
2020-03-31 22:46:02 +0200

    learning juicypixels + data folder

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
 import Hopfield
+import Codec.Picture
+import Data.Either
+import Control.Applicative
+
+pairs :: [a] -> [b] -> [(a,b)]
+pairs as bs = (,) <$> as <*> bs
+
+pixelsAsList :: Image PixelRGB8 -> [Int]
+pixelsAsList img = map go $ pairs [0..w-1] [0..h-1]
+  where w = imageWidth img
+        h = imageHeight img
+        go (x, y) = pixelToBinary $ pixelAt img x y
+        pixelToBinary (PixelRGB8 r g b) = 0
 
 main :: IO ()
-main = putStrLn "hello"
+main = do
+  img <- readImage "data/test.png"
+  let rgbImg = convertRGB8 <$> img
+  putStrLn . show . length $ imageData <$> rgbImg
