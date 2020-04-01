d895f494441296be05e8f5f147ac23b5dcec6919
Matej Fandl <fandl@mladypes.sk>
2020-04-01 21:35:28 +0200

    pattern matching instead of local bindings

--- app/Main.hs
+++ app/Main.hs
@@ -9,10 +9,8 @@ pairs :: [a] -> [b] -> [(a, b)]
 pairs as bs = (,) <$> as <*> bs
 
 pixelsAsList :: Image PixelRGB8 -> [Int]
-pixelsAsList img = map go $ pairs [0..w-1] [0..h-1]
-  where w = imageWidth img
-        h = imageHeight img
-        go (x, y) = pixelToBinary $ pixelAt img x y
+pixelsAsList img@(Image w h _) = map go $ pairs [0..w-1] [0..h-1]
+  where go (x, y) = pixelToBinary $ pixelAt img x y
         pixelToBinary (PixelRGB8 r g b) = 0
 
 main :: IO ()
